f97b0c8310afc9d977c3cd46b9e69f14149e196b
Igor Coelho <74683510+whothekid@users.noreply.github.com>
2025-06-19 22:29:40 -0300

    Create initials.py
    Main python program file.

---
+++ initials.py
@@ -0,0 +1,21 @@
+# Igor Coelho da Costa
+# My nickname is Coelho (rabbit)
+
+space1 = " "
+space2 = "  "
+space3 = "   "
+space4 = "    "
+space5 = "     "
+
+my_initial = "I"
+my_initial5 = "IIIII"
+family_initial = "C"
+family_initial3 = "CCC"
+
+print(my_initial5 + space3 + family_initial3 + """
+""" + space2 + my_initial + space4 + family_initial + """
+""" + space2 + my_initial + space4 + family_initial + """
+""" + space2 + my_initial + space4 + family_initial + """
+""" + space2 + my_initial + space4 + family_initial + """
+""" + space2 + my_initial + space4 + family_initial + """
+""" + my_initial5 +space3 + family_initial3)
