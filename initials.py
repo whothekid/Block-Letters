bd575d185e5465ba8b8583c0cb529d086db51343
Igor Coelho <74683510+whothekid@users.noreply.github.com>
2025-06-19 22:37:42 -0300

    Update initials.py
    Improved code effiency by changing string concatenation for a loop to generate the repeated parts of the ASCII art.

--- initials.py
+++ initials.py
@@ -1,21 +1,19 @@
 # Igor Coelho da Costa
 # My nickname is Coelho (rabbit)
 
+#Define spaces for formatting
 space1 = " "
 space2 = "  "
 space3 = "   "
 space4 = "    "
 space5 = "     "
 
-my_initial = "I"
-my_initial5 = "IIIII"
-family_initial = "C"
-family_initial3 = "CCC"
+#Define initials
+myInitial = "I"
+myInitial5 = "IIIII"
+familyInitial = "C"
+familyInitial3 = "CCC"
 
-print(my_initial5 + space3 + family_initial3 + """
-""" + space2 + my_initial + space4 + family_initial + """
-""" + space2 + my_initial + space4 + family_initial + """
-""" + space2 + my_initial + space4 + family_initial + """
-""" + space2 + my_initial + space4 + family_initial + """
-""" + space2 + my_initial + space4 + family_initial + """
-""" + my_initial5 +space3 + family_initial3)
+for i in range(5):
+    print(space2 + myInitial + space4 + familyInitial)
+print(myInitial5 + space3 + familyInitial3)
